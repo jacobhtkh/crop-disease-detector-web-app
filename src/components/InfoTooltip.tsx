79f0c26133e9dd2b15b1e8f349c81b6f8dcdaa9c
Jacob Ujanga <jacob.ujanga@gmail.com>
2026-04-22 20:40:36 +0100

    make tooltip width sensible

--- src/components/InfoTooltip.tsx
+++ src/components/InfoTooltip.tsx
@@ -5,7 +5,7 @@ export const InfoTooltip = ({ text }: Props) => (
     <span className='text-gray-400 hover:text-gray-600 cursor-default text-xs select-none'>
       ⓘ
     </span>
-    <span className='absolute left-0 bottom-full mb-1.5 w-60 bg-gray-800 text-white text-xs rounded-lg p-2.5 hidden group-hover:block z-10 leading-relaxed shadow-lg'>
+    <span className='absolute left-0 bottom-full mb-1.5 w-max max-w-xs bg-gray-800 text-white text-xs rounded-lg p-2.5 hidden group-hover:block z-10 leading-relaxed shadow-lg'>
       {text}
     </span>
   </span>
